87f61150c59274b2060d8d6d45a0e7fcf45c5d95
NikoArc42 <nikoarc42@gmail.com>
2025-10-27 20:24:48 +0100

    great

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const main = document.querySelector("main");
 
+function createGameCard(rank, title, playerCount, peak24, alltimePeak) {
+  const card = document.createElement("section");
 
-function createGameCard(rank, title, playerCount, twentyFourHourPeak, alltimePeak) {
-    const card = document.createElement("section");
-    
-};
+  const titleElement = document.createElement("h2");
+  const playerCountElement = document.createElement("div");
+  const peak24Element = document.createElement("div");
+  const alltimePeakElement = document.createElement("div");
+
+  card.appendChild(titleElement);
+  card.appendChild(playerCountElement);
+  card.appendChild(peak24Element);
+  card.appendChild(alltimePeakElement);
+
+  main.appendChild(card)
+}
+
+createGameCard(34, "Gavin", 608, 608, 100054);
