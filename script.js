c964e091e0480b55200715315817b481f876bbe8
NikoArc42 <nikoarc42@gmail.com>
2025-10-27 19:54:44 +0100

    g

---
+++ script.js
@@ -0,0 +1,7 @@
+const main = document.querySelector("main");
+
+
+function createGameCard(rank, title, playerCount, twentyFourHourPeak, alltimePeak) {
+    const card = document.createElement("section");
+    
+};
